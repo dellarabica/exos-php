2a139aac7988f63e67b7d3501568b9f9fea69d38
Benoît // Della <63121195+dellarabica@users.noreply.github.com>
2021-01-11 17:02:52 +0100

    ajout exo damier

--- functions.php
+++ functions.php
@@ -15,4 +15,60 @@ function Palindrome($str, $strrev){
     $str2 = strtolower($strrev);
     echo ($str1 == $str2) ? "<br/>La chaîne est donc un palindrome" : "<br/>La chaîne n'est donc pas un palindrome";
 }
+
+function GenBoard($l,$c){
+    //true = noir - false = blanc
+    $case = true;
+
+    $damier = [];
+    for ($i=0;$i<$l; $i++){
+
+        for ($j=0;$j<$c; $j++){
+
+            switch($case){
+
+                case true:
+                    $damier[$i][$j] = 1;
+                    $case = false;
+                    break;
+
+                case false:
+                    $damier[$i][$j] = 0;
+                    $case = true;
+                    break;
+
+                default:
+                    break;
+
+            }
+            if($l%2 == 0 && $c%2 == 0){
+                if($i%2 != 0)
+                {
+                    $damier[$i][$j] = !$damier[$i][$j];
+                }           
+            }
+
+        }
+
+    }
+    return $damier;
+}
+
+function DisplayBoard($board){
+echo "<div class='board'>";
+for ($i=0;$i<count($board); $i++){
+    echo "<div class='line'>";
+    foreach($board[$i] as $k => $v ){
+        if($v == 0){
+            echo "<div class='white'></div>";
+        }
+        else
+        {
+            echo "<div class='black'></div>";
+        }
+    }
+    echo "</div>";
+}
+echo "</div>";
+}
 ?>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Exercices en PHP</title>
         <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="/style.css">
     </head>
 <body>
     <?php
@@ -12,6 +13,14 @@
     echo "La chaîne originale est ".$str;
     echo "<br/>La chaîne inversée est ".$strrev;
     Palindrome($str, $strrev);
+    echo "<br/>"; //Séparation inutile
+
+    $l = 11;
+    $c = 11;
+    
+    $board = GenBoard($l,$c);
+    //var_dump($board);
+    DisplayBoard($board);
     ?>
 </body>
 </html>
