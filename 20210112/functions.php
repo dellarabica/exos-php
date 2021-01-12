0ca8ab9a0b56affaf709bb567bf042f2250fe182
Benoît // Della <63121195+dellarabica@users.noreply.github.com>
2021-01-12 12:47:06 +0100

    exos 1 à 4

--- 20210112/functions.php
+++ 20210112/functions.php
@@ -38,20 +38,88 @@ function GenBoard($l,$c){
 }
 
 function DisplayBoard($board){
-echo "<div class='board'>";
-for ($i=0;$i<count($board); $i++){
-    echo "<div class='line'>";
-    foreach($board[$i] as $k => $v ){
-        if($v == 0){
-            echo "<div class='white'></div>";
+    echo "<div class='board'>";
+    for ($i=0;$i<count($board); $i++){
+        echo "<div class='line'>";
+        foreach($board[$i] as $k => $v ){
+            if($v == 0){
+                echo "<div class='white'></div>";
+            }
+            else
+            {
+                echo "<div class='black'></div>";
+            }
         }
-        else
-        {
-            echo "<div class='black'></div>";
-        }
+    echo "</div>";
     }
     echo "</div>";
 }
-echo "</div>";
+
+function RandArray($num){
+    $nbVal = $num;
+    $array = [];
+    for($i = 0; $i < $nbVal; $i++){
+        $array[$i] = random_int(1, 999999);
+    }
+    return $array;
+}
+
+function DisplayArray($array){
+    $rows = array();
+        $cells = array();
+        for ($ii = 0; $ii < count($array); $ii++) {
+            $data = $array[$ii];
+            $cells[] = "<td>{$data}</td>";
+        }
+        $rows[] = "<tr>" . implode('', $cells) . "</tr>";
+    $table = "<table>" . implode('', $rows) . "</table>";
+    echo $table;
+}
+
+function SortArray($nbitem, $sorttype){
+    $arraybase = RandArray($nbitem);
+    switch($sorttype)
+    {
+        case "ASC":
+
+            sort($arraybase);
+            break;
+
+        case "DESC":
+            rsort($arraybase);
+            break;
+
+        default:
+            break;
+    }
+    return $arraybase;
+}
+
+function GenMulti($l, $c, $sort){
+    $mult = array();
+    for ($i=0;$i<$l; $i++){
+        $array = RandArray($c);
+        $arrayS = SortArray($array, $sort);
+        array_push($mult, $arrayS);
+        echo var_dump($mult);
+    }
+    
+   return $mult;
+}
+
+function DisplayMultiArray($mult){
+    $rows = array();
+    for($i=0;$i<count($mult);$i++){
+        $cells = array();
+        $ds = $mult[$i];
+        for ($ii = 0; $ii < count($mult[$i]); $ii++) {
+            $data = $ds[$ii];
+            $cells[] = "<td>{$data}</td>";
+        }
+        $rows[] = "<tr>" . implode('', $cells) . "</tr>";
+    }
+    echo var_dump($rows);
+    $table = "<table>" . implode('', $rows) . "</table>";
+    echo $table;
 }
 ?>
--- 20210112/index.php
+++ 20210112/index.php
@@ -6,14 +6,33 @@
         <link rel="stylesheet" type="text/css" href="/style.css">
     </head>
 <body>
-    <?php
+<?php
     require_once('functions.php');
-   echo "<p>Exercice 1</p><br/>";
-   $l = 11; // nombre de lignes
-   $c = 11; // nombre de colonnes
-   
+
+   echo "<p>Exercice 1</p>";
+   $l = 6; // nombre de lignes
+   $c = 6; // nombre de colonnes
    $board = GenBoard($l,$c); //on génère le damier
    DisplayBoard($board); //on affiche le damier
-    ?>
+
+   echo "<br/><p>Exercice 2</p>";
+   $num = 6; //Nombre d'éléments dans notre array
+   $random = RandArray($num); //On génére des nombres entiers positifs aléatoires
+   DisplayArray($random); //On les affiche
+
+   echo "<br/><p>Exercice 3</p>";
+   $num_2 = 8; //Nombre d'items
+   $sort = "ASC"; //Comment les nombres seront triés (ASC / DESC)
+   $arrayS = SortArray($num_2, $sort); //On génère et on trie
+   DisplayArray($arrayS); //On les affiche
+
+   echo "<br/><p>Exercice 4</p>";
+   $lin = 6;
+   $col = 6;
+   $sort = "ASC"; //Comment les nombres seront triés (ASC / DESC)
+   $multi = GenMulti($lin, $col, $sort); //On génère un tableau multidimensionnel et on trie chaque ligne
+   DisplayMultiArray($multi);
+
+?>
 </body>
 </html>
